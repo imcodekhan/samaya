597473bfd604e7b0ac816587e1b0e6a6f8543617
imcodekhan <imkhan.mdamir@outlook.com>
2023-06-13 02:34:55 +0530

    mantine setup

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mantine/core";
+import "./styles.css";
+
+const Dashboard = () => {
+  return <Container className="dashboard"></Container>;
+};
+
+export default Dashboard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Dashboard from "./Pages/Dashboard";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>Home</React.StrictMode>
+  <React.StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Dashboard />
+    </MantineProvider>
+  </React.StrictMode>
 );
